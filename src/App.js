ea10874facabfcce4b528d238709468ec02ed833
korayyersel <koray.yersel@q-perior.com>
2020-10-30 17:01:12 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+
+import { Auth, Hub } from 'aws-amplify'
+
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+    <button onClick={checkUser}>Check User</button>
+    </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
